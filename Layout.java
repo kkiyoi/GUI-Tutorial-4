c256150df001d64de7ececcfbee12e1c00154742
kkiyoi <30498702+kkiyoi@users.noreply.github.com>
2024-09-13 18:57:39 -0700

    Update Layout.java

--- Layout.java
+++ Layout.java
@@ -3,6 +3,15 @@ import java.awt.event.*;
 import java.awt.*;
 import java.awt.Color;
 
+// import javax.swing.*;
+// import java.awt.event.*;
+// import javax.swing.JFrame;
+// import javax.swing.JLabel;
+// import javax.swing.ImageIcon;
+// import java.awt.*;
+// import java.awt.Color;
+// import javax.swing.border.Border;
+// import javax.swing.BorderFactory;
 
 public class Layout {
  Layout() {
