33f33b774c2ba1a932faca1839476c1146da938d
kkiyoi <30498702+kkiyoi@users.noreply.github.com>
2024-09-13 18:51:42 -0700

    Initial commit

---
+++ MyContainers.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.Color;
+
+
+public class MyContainers {
+  MyContainers() {
+
+//JPanel = a GUI component that functions as a container to hold other components 
+
+
+
+	//Centering the image and text inside the label	
+
+
+
+		
+	//if you want to fine tune where inside the container the image is uncomment below
+// label.setBounds(100,75,75, 75); //x coordinate, y coordinate, x width, y width	
+
+//red panel square
+
+		
+//blue panel square
+
+
+//green panel rectangle
+
+
+//Allows you to fine tune where the image will be inside the label		
+	//GUI Basic codes	
+
+
+		
+//add the three colored boxes to the frame
+
+//add the label to the frame
+
+
+	}
+}
