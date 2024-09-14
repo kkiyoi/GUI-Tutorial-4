3d4514ff2883701b8742994a55c62bbf7f577910
kkiyoi <30498702+kkiyoi@users.noreply.github.com>
2024-09-13 18:56:07 -0700

    Update and rename MyContainers.java to Layout.java

---
+++ Layout.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.Color;
+
+
+public class Layout {
+ Layout() {
+
+//Basics of a GUI
+
+
+
+//creates five panels which will be assigned to different places in the frame
+
+
+
+		
+//set the background colors of each panel
+
+	 
+
+//sets the initial size of the boxes
+
+		
+//------------- subpanels for the center -----------
+
+
+
+//set the background colors of each panel
+
+
+
+//sets the initiall size of the boxes
+
+
+
+//Assign locations for each of the panels. 
+
+
+//--------------- end subpanels ----------------
+
+//Assign locations for each of the panels
+
+
+
+	}
+}
